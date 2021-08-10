37a94eb4a6a763e019f3d1bf6e7035500852a4ea
Mazagames <80614728+Mazagames@users.noreply.github.com>
2021-08-10 09:13:51 +0530

    Add files via upload

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -5,7 +5,7 @@
 {
 	const scriptsInEvents = {
 
-		async Mainevents_Event46_Act3(runtime, localVars)
+		async Mainevents_Event48_Act2(runtime, localVars)
 		{
 			if (typeof gameManager !== 'undefined') {
 			var data = gameManager.onGameInit()
@@ -13,9 +13,19 @@
 			 
 			    var obj = {
 			       userID: data.userId,
-				   score: runtime.globalVars.SCORE,
+				    gameID: data.gameId,
+			        roomID: data.roomId,
+				   currentScore: runtime.globalVars.SCORE,
 					highScore: runtime.globalVars.HIGHSCORE,
-			        type: "over"
+					playTime: runtime.globalVars.gametime,
+			        type: "over",
+					adGameStartOpportunity:runtime.globalVars.adGameStartOpportunity,
+					adGameStartShown:runtime.globalVars.adGameStartShown,
+					adGameStartClaimed:runtime.globalVars.adGameStartClaimed,
+					adGameEndOpportunity:runtime.globalVars.adGameEndOpportunity,
+					adGameEndShown:runtime.globalVars.adGameEndShown,
+					adGameEndClaimed:runtime.globalVars.adGameEndClaimed,
+					adGamePowerupClaimed:0
 			    }
 			    try {
 			        var data = JSON.stringify(obj)
@@ -26,7 +36,7 @@
 			}
 		},
 
-		async Mainevents_Event46_Act5(runtime, localVars)
+		async Mainevents_Event48_Act4(runtime, localVars)
 		{
 			if (typeof gameManager !== 'undefined') {
 			var data = gameManager.onGameInit()
@@ -50,7 +60,62 @@
 			}
 		},
 
-		async Mainevents_Event58_Act4(runtime, localVars)
+		async Mainevents_Event49_Act2(runtime, localVars)
+		{
+			if (typeof gameManager !== 'undefined') {
+			var data = gameManager.onGameInit()
+			 data = JSON.parse(data)
+			 
+			    var obj = {
+			       userID: data.userId,
+				    gameID: data.gameId,
+			        roomID: data.roomId,
+				   currentScore: runtime.globalVars.SCORE,
+					highScore: runtime.globalVars.HIGHSCORE,
+					playTime: runtime.globalVars.gametime,
+			        type: "over",
+					adGameStartOpportunity:runtime.globalVars.adGameStartOpportunity,
+					adGameStartShown:runtime.globalVars.adGameStartShown,
+					adGameStartClaimed:runtime.globalVars.adGameStartClaimed,
+					adGameEndOpportunity:runtime.globalVars.adGameEndOpportunity,
+					adGameEndShown:runtime.globalVars.adGameEndShown,
+					adGameEndClaimed:runtime.globalVars.adGameEndClaimed,
+					adGamePowerupClaimed:0
+			    }
+			    try {
+			        var data = JSON.stringify(obj)
+			        gameManager.onTrack('gameExit', data)
+			    } catch (e) {
+			        gameManager.onError(e.stack.toString())
+			    }
+			}
+		},
+
+		async Mainevents_Event49_Act4(runtime, localVars)
+		{
+			if (typeof gameManager !== 'undefined') {
+			var data = gameManager.onGameInit()
+			 data = JSON.parse(data)
+			 
+			    var obj = {
+			       gameID: data.gameId,
+			        roomID: data.roomId,
+			        userID: data.userId,
+					score: runtime.globalVars.SCORE,
+					highScore: runtime.globalVars.HIGHSCORE,
+			        info: encryption.getInfo(runtime.globalVars.SCORE, 10, 1)
+					
+			    }
+			    try {
+			        var score = JSON.stringify(obj)
+			        gameManager.onGameOver(score)
+			    } catch (e) {
+			        gameManager.onError(e.stack.toString())
+			    }
+			}
+		},
+
+		async Mainevents_Event64_Act4(runtime, localVars)
 		{
 			if (typeof gameManager !== 'undefined') {
 			
@@ -69,6 +134,7 @@
 					type: "Pause",
 					score: runtime.globalVars.SCORE,
 			        highScore: runtime.globalVars.HIGHSCORE,
+					currentTime:runtime.globalVars.pausetimer,
 					
 					}
 							
@@ -86,7 +152,71 @@
 			}
 		},
 
-		async Mainevents_Event246_Act3(runtime, localVars)
+		async Mainevents_Event248_Act1(runtime, localVars)
+		{
+			if (typeof gameManager !== 'undefined') {
+			var data = gameManager.onGameInit()
+			 data = JSON.parse(data)
+			 
+			    var obj = {
+			       gameID: data.gameId,
+			        roomID: data.roomId,
+			        userID: data.userId,
+					position : "end"
+				    }
+			    try {
+			        var data = JSON.stringify(obj)
+			        gameManager.onTrack('gameAdShown', data)
+			    } catch (e) {
+			        gameManager.onError(e.stack.toString())
+			    }
+			}
+		},
+
+		async Mainevents_Event249_Act2(runtime, localVars)
+		{
+			if (typeof gameManager !== 'undefined') {
+			var data = gameManager.onGameInit()
+			 data = JSON.parse(data)
+			 
+			    var obj = {
+			       gameID: data.gameId,
+			        roomID: data.roomId,
+			        userID: data.userId,
+					position : "end"
+				    }
+			    try {
+			        var data = JSON.stringify(obj)
+			        gameManager.onTrack('gameAdShown', data)
+			    } catch (e) {
+			        gameManager.onError(e.stack.toString())
+			    }
+			}
+		},
+
+		async Mainevents_Event249_Act3(runtime, localVars)
+		{
+			if (typeof gameManager !== 'undefined') {
+			var data = gameManager.onGameInit()
+			 data = JSON.parse(data)
+			 
+			    var obj = {
+			       gameID: data.gameId,
+			        roomID: data.roomId,
+			        userID: data.userId,
+					position : "end",
+					autoPlayed : 0
+				    }
+			    try {
+			        var data = JSON.stringify(obj)
+			        gameManager.onTrack('gameAdClicked', data)
+			    } catch (e) {
+			        gameManager.onError(e.stack.toString())
+			    }
+			}
+		},
+
+		async Mainevents_Event250_Act1(runtime, localVars)
 		{
 			if (typeof gameManager !== 'undefined' && 
 			typeof gameManager.onShowRewardedVideoAds === 'function'
@@ -99,13 +229,31 @@
 				     if (result.status === 0) {
 					 		
 					 runtime.globalVars.SCORE *= 2; 
+					 runtime.globalVars.adGameEndClaimed += 1; 
 					
 					 runtime.globalVars.adsound = 0; 
 					 // (runtime.globalVars.SCORE >= runtime.globalVars.HIGHSCORE) == runtime.globalVars.HIGHSCORE;
 					 
 					 (runtime.callFunction('gameover', [0,1]))
 					
-				   
+			if (typeof gameManager !== 'undefined') {
+			var data = gameManager.onGameInit()
+			 data = JSON.parse(data)
+			 
+			    var obj = {
+			       gameID: data.gameId,
+			        roomID: data.roomId,
+			        userID: data.userId,
+					position : "end",
+				autoPlayed : 0
+				    }
+			    try {
+			        var data = JSON.stringify(obj)
+			        gameManager.onTrack('gameAdClaimed', data)
+			    } catch (e) {
+			        gameManager.onError(e.stack.toString())
+			    }
+			}
 				
 				   
 			    } else {
@@ -126,7 +274,50 @@
 			
 		},
 
-		async Upgrade_Event40_Act2(runtime, localVars)
+		async Upgrade_Event40_Act3(runtime, localVars)
+		{
+			if (typeof gameManager !== 'undefined') {
+			var data = gameManager.onGameInit()
+			 data = JSON.parse(data)
+			 
+			    var obj = {
+			       gameID: data.gameId,
+			        roomID: data.roomId,
+			        userID: data.userId,
+					position : "start"
+				    }
+			    try {
+			        var data = JSON.stringify(obj)
+			        gameManager.onTrack('gameAdShown', data)
+			    } catch (e) {
+			        gameManager.onError(e.stack.toString())
+			    }
+			}
+		},
+
+		async Upgrade_Event40_Act4(runtime, localVars)
+		{
+			if (typeof gameManager !== 'undefined') {
+			var data = gameManager.onGameInit()
+			 data = JSON.parse(data)
+			 
+			    var obj = {
+			       gameID: data.gameId,
+			        roomID: data.roomId,
+			        userID: data.userId,
+					position : "start",
+					autoPlayed : 0
+				    }
+			    try {
+			        var data = JSON.stringify(obj)
+			        gameManager.onTrack('gameAdClicked', data)
+			    } catch (e) {
+			        gameManager.onError(e.stack.toString())
+			    }
+			}
+		},
+
+		async Upgrade_Event40_Act5(runtime, localVars)
 		{
 			if (typeof gameManager !== 'undefined' && 
 			typeof gameManager.onShowRewardedVideoAds === 'function'
@@ -139,9 +330,29 @@
 				     if (result.status === 0) {
 					 		
 					 runtime.globalVars.missileon += 1; 
-			
+					  runtime.globalVars.adGameStartClaimed += 1; 
 			runtime.globalVars.adsound = 0; 
-			    } else {
+			
+			if (typeof gameManager !== 'undefined') {
+			var data = gameManager.onGameInit()
+			 data = JSON.parse(data)
+			 
+			    var obj = {
+			       gameID: data.gameId,
+			        roomID: data.roomId,
+			        userID: data.userId,
+					position : "start",
+				autoPlayed : 0
+				    }
+			    try {
+			        var data = JSON.stringify(obj)
+			        gameManager.onTrack('gameAdClaimed', data)
+			    } catch (e) {
+			        gameManager.onError(e.stack.toString())
+			    }
+			}
+			
+			} else {
 			        // did not watch the ad completely. no reward
 				//runtime.globalVars.reward = runtime.globalVars.SCORE + 0;
 					 runtime.globalVars.missileon += 0; 
